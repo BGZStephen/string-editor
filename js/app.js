5ce758190bc20f5b9eb71ad0806ab3834cebdf74
BGZStephen <sjw948@gmail.com>
2017-02-14 20:32:08 +0000

    refactor to arrow function syntax and let / const variables

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-var userInput = document.getElementById("user-input");
-var removeVowelsButton = document.getElementById("remove-vowels")
-var removeSpacesButton = document.getElementById("remove-spaces")
-var outputElement = document.getElementById("output-container")
-var outputString;
+let userInput = document.getElementById("user-input");
+const removeVowelsButton = document.getElementById("remove-vowels")
+const removeSpacesButton = document.getElementById("remove-spaces")
+const outputElement = document.getElementById("output-container")
+let outputString;
 
-var userString; // define global variable for user string
+let userString; // define global variable for user string
 
-userInput.addEventListener("keyup", function() {
+userInput.addEventListener("keyup", () => {
   userString = document.getElementById("user-input").value;
 })
 
-removeVowelsButton.onclick = function() {
+removeVowelsButton.onclick = () => {
   outputString = ""
   outputString = "<p>Output: "
   outputString += userString.replace(/[aeiou]/gi,"")
   outputElement.innerHTML = outputString + "</p>"
 }
 
-removeSpacesButton.onclick = function() {
+removeSpacesButton.onclick = () => {
   outputString = ""
   outputString = "<p>Output: "
   outputString += userString.replace(/ /gi,"")
